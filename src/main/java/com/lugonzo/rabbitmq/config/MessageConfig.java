6b566636d2bc028fa41213a162d93c5b2b6ad402
Abuu001 <abulugz@gmail.com>
2021-08-09 00:20:46 +0300

    config for rabbitMQ

--- src/main/java/com/lugonzo/rabbitmq/config/MessageConfig.java
+++ src/main/java/com/lugonzo/rabbitmq/config/MessageConfig.java
@@ -1,7 +1,7 @@
 package com.lugonzo.rabbitmq.config;
 
-import com.rabbitmq.client.ConnectionFactory;
 import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
@@ -35,7 +35,7 @@ public class MessageConfig {
 
    @Bean
     public AmqpTemplate template(ConnectionFactory connectionFactory){
-        final RabbitTemplate rabbitTemplate = new RabbitTemplate((org.springframework.amqp.rabbit.connection.ConnectionFactory) connectionFactory);
+        final RabbitTemplate rabbitTemplate = new RabbitTemplate( connectionFactory);
         rabbitTemplate.setMessageConverter(converter());
         return  rabbitTemplate;
    }
