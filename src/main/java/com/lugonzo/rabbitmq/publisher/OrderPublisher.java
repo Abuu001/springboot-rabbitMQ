91e89bbe88087b1f9f1711c5fae2c0a3163c8669
Abuu001 <abulugz@gmail.com>
2021-08-09 00:22:31 +0300

    rabbitMQ publisher

--- src/main/java/com/lugonzo/rabbitmq/publisher/OrderPublisher.java
+++ src/main/java/com/lugonzo/rabbitmq/publisher/OrderPublisher.java
@@ -1,2 +1,42 @@
-package com.lugonzo.rabbitmq.publisher;public class OrderPublisher {
+package com.lugonzo.rabbitmq.publisher;
+
+import com.lugonzo.rabbitmq.config.MessageConfig;
+import com.lugonzo.rabbitmq.dto.Order;
+import com.lugonzo.rabbitmq.dto.OrderStatus;
+import com.lugonzo.rabbitmq.dto.Status;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@Slf4j
+@RequestMapping("/api/v1/order")
+@RestController
+public class OrderPublisher {
+
+    @Autowired
+    private RabbitTemplate template;
+
+    @GetMapping
+    public String bb(){
+        return "Abuu";
+    }
+
+    @PostMapping
+    public String bookOrder(@RequestBody Order newOrder){
+        newOrder.setOrderId(UUID.randomUUID().toString());
+        var orderStatus =  OrderStatus.builder()
+                .order(newOrder)
+                .status(Status.PROCESS)
+                .message("order placed successfully  !!! 👍👍👍")
+                .build();
+
+        log.info(orderStatus.toString());
+
+        template.convertAndSend(MessageConfig.EXCHANGE,MessageConfig.ROUTING_KEY,orderStatus);
+        return "Success !!! 🥳🥳🥳";
+    }
+
 }
