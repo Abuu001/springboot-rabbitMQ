e1ea602d59fcf5c418662655def1144ca359c447
Abuu001 <abulugz@gmail.com>
2021-08-09 00:21:28 +0300

    order props

--- src/main/java/com/lugonzo/rabbitmq/dto/Order.java
+++ src/main/java/com/lugonzo/rabbitmq/dto/Order.java
@@ -1,4 +1,19 @@
 package com.lugonzo.rabbitmq.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Order {
+    private String orderId;
+    private String name;
+    private int quantity;
+    private double price;
 }
