f8589fb68f50496f96039220b46196cd1d822324
Abuu001 <abulugz@gmail.com>
2021-08-09 00:21:03 +0300

    consumer

--- src/main/java/com/lugonzo/rabbitmq/consumer/User.java
+++ src/main/java/com/lugonzo/rabbitmq/consumer/User.java
@@ -1,2 +1,15 @@
-package com.lugonzo.rabbitmq.consumer;public class User {
+package com.lugonzo.rabbitmq.consumer;
+
+import com.lugonzo.rabbitmq.config.MessageConfig;
+import com.lugonzo.rabbitmq.dto.OrderStatus;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class User {
+
+    @RabbitListener(queues = MessageConfig.QUEUE)
+    public void consumerMessageFromQueue(OrderStatus orderStatus){
+        System.out.println("Message received from queue  : "+orderStatus);
+    }
 }
