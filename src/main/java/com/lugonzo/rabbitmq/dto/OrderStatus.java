53ff8199ab7396c6fe9690ed136e3307e4ccda29
Abuu001 <abulugz@gmail.com>
2021-08-09 00:21:46 +0300

    order status

--- src/main/java/com/lugonzo/rabbitmq/dto/OrderStatus.java
+++ src/main/java/com/lugonzo/rabbitmq/dto/OrderStatus.java
@@ -1,4 +1,16 @@
 package com.lugonzo.rabbitmq.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class OrderStatus {
+    private Order order;
+    private Status status;
+    private String message;
 }
