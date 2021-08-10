1711125ed26ea401b010123c6dca5f0f7223d093
Abuu001 <abulugz@gmail.com>
2021-08-10 12:07:40 +0300

    Employee props

--- src/main/java/com/lugonzo/rabbitmq/dto/Employee.java
+++ src/main/java/com/lugonzo/rabbitmq/dto/Employee.java
@@ -1,2 +1,17 @@
-package com.lugonzo.rabbitmq.dto;public class Employee {
+package com.lugonzo.rabbitmq.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Employee {
+    private String empId;
+     private String empName;
+     private Integer age;
+     private Gender gender;
 }
