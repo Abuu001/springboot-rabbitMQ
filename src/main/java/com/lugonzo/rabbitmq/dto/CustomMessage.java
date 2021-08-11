297209747a35b0ca9b510b03cf740719a5c16094
Abuu001 <abulugz@gmail.com>
2021-08-11 21:15:50 +0300

    messege props

--- src/main/java/com/lugonzo/rabbitmq/dto/CustomMessage.java
+++ src/main/java/com/lugonzo/rabbitmq/dto/CustomMessage.java
@@ -1,2 +1,19 @@
-package com.lugonzo.rabbitmq.dto;public class CustomMessage {
+package com.lugonzo.rabbitmq.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CustomMessage {
+
+    private String messageId;
+    private String message;
+    private Date messageDate;
 }
