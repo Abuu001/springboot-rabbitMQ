a5aae71f2c0196a875f06ab365ea8d45cebcf2fa
Abuu001 <abulugz@gmail.com>
2021-08-11 21:16:07 +0300

    publisher

--- src/main/java/com/lugonzo/rabbitmq/publisher/MessagePublisher.java
+++ src/main/java/com/lugonzo/rabbitmq/publisher/MessagePublisher.java
@@ -1,2 +1,34 @@
-package com.lugonzo.rabbitmq.publisher;public class MessagePublisher {
+package com.lugonzo.rabbitmq.publisher;
+
+import com.lugonzo.rabbitmq.config.MQConfig;
+import com.lugonzo.rabbitmq.dto.CustomMessage;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v1/message")
+public class MessagePublisher {
+
+    @Autowired
+    public RabbitTemplate rabbitTemplate;
+
+    @PostMapping("/publish")
+    public String publishMessage(@RequestBody CustomMessage newMessage){
+        newMessage.setMessageId(UUID.randomUUID().toString());
+        newMessage.setMessageDate(new Date());
+
+        rabbitTemplate.convertAndSend(MQConfig.EXCHANGE,MQConfig.ROUTING_KEY,newMessage);
+
+        return "Message Published 🚀🚀🚀";
+    }
 }
