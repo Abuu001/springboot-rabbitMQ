3f3f31c3813b89f8369e88547f06ae58f28a3d27
Abuu001 <abulugz@gmail.com>
2021-08-11 21:15:31 +0300

    rabbit listener

--- src/main/java/com/lugonzo/rabbitmq/consumer/MessageListener.java
+++ src/main/java/com/lugonzo/rabbitmq/consumer/MessageListener.java
@@ -1,2 +1,15 @@
-package com.lugonzo.rabbitmq.consumer;public class MessageListener {
+package com.lugonzo.rabbitmq.consumer;
+
+import com.lugonzo.rabbitmq.config.MQConfig;
+import com.lugonzo.rabbitmq.dto.CustomMessage;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MessageListener {
+
+    @RabbitListener(queues = MQConfig._QUEUE    )
+    public void listener(CustomMessage message){
+        System.out.println(message);
+    }
 }
